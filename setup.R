a936b5c0d2ee4814e7a9c56e23788aab42a7d348
Katie Larkin <katie.larkin@colostate.edu>
2026-01-02 16:16:23 -0700

    initial

---
+++ setup.R
@@ -0,0 +1,34 @@
+# Set up script
+
+
+# function to check for package installation, then install and load libraries
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+
+# vector of packages to load
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgbif',
+              'soilDB',
+              'plotly',
+              'mapview',
+              "leaflet",
+              'gifski',
+              'grid')
+
+packageLoad(packages)
+
